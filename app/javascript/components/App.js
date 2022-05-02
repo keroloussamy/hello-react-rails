1b222d5e30844fc8b0f73df722c6e1849d6846a7
keroloussamy <keroloussamy98@gmail.com>
2022-05-03 00:07:25 +0200

    Update App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,18 @@
 import React from "react"
-import { BrowserRouter } from "react-router-dom";
-import HelloWorld from "./HelloWorld";
-class App extends React.Component {
-  render () {
-    return (
-      <BrowserRouter>
-        <HelloWorld greeting="Hello from react-rails." />
+import Greeting from "./Greeting";
+import store from '../redux/configureStore';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+      <Route exact path="" element={<Greeting />} />
+      </Routes>
     </BrowserRouter>
-    );
-  }
-}
+  </Provider>
+);
+
 
 export default App
