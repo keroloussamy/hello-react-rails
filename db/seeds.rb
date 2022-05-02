b044ba5a1d803f163eae3b1bc59b641a2d598832
keroloussamy <keroloussamy98@gmail.com>
2022-05-02 22:18:09 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create!(
+  id: 1, 
+  text: "Hello 1"
+)
+Greeting.create!(
+  id: 2, 
+  text: "Hello 2"
+)
+Greeting.create!(
+  id: 3, 
+  text: "Hello 3"
+)
+Greeting.create!(
+  id: 4, 
+  text: "Hello 4"
+)
+Greeting.create!(
+  id: 5, 
+  text: "Hello 5"
+)
